15672951e5dcc97d66ffba54cb32cc18c1056eba
Ziggy-Github <162323407+Kurtjex@users.noreply.github.com>
2024-05-29 01:03:08 +0800

    Create spam.js

---
+++ modules/commands/spam.js
@@ -0,0 +1,24 @@
+module.exports = {
+  config: {
+name: "spam",
+  version: "",
+  role: 1,
+  author: "kurt",
+  description: "",
+  usePrefix: true,
+  usage: "",
+  cooldown: 5,
+},
+
+async onRun({ message, event, }) {
+  var { threadID, } = event;
+  var k = function (k) { message.reply(k, threadID)};
+
+  //*vonglap
+
+for (i = 0; i < 10; i++) {
+ k("[⚠️] SAPAM");
+}
+
+  }
+  };
